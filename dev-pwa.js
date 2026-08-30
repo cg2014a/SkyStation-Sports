70a451c6dce050599c5c5a2ab33a58a7aef9fed2
cg2014a <cgarrett4@hotmail.com>
2026-08-30 17:36:52 -0500

    update

--- dev-pwa.js
+++ dev-pwa.js
@@ -1,4 +1,6 @@
 // Localhost should always use the newest source files. Production PWA caching is untouched.
 if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
   navigator.serviceWorker?.getRegistrations().then(registrations => Promise.all(registrations.map(registration => registration.unregister()))).then(() => console.info('[SkyStation Sports] Local development service worker disabled.')).catch(error => console.warn('[SkyStation Sports] Could not disable local service worker', error));
+} else if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js', { updateViaCache: 'none' }).then(registration => registration.update()).catch(error => console.warn('[SkyStation Sports] Service worker registration failed', error));
 }
