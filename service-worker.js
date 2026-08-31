2b5232f0a58218411be0e5c53abd80daa3fe6245
cg2014a <cgarrett4@hotmail.com>
2026-08-31 18:59:36 -0500

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'skystation-sports-v1.0.11';
+const VERSION = 'skystation-sports-v1.0.12';
 const ROOT = new URL('./', self.registration.scope).href;
 const SHELL = [ROOT, './index.html', './style.css', './app.js', './dev-pwa.js', './manifest.webmanifest', './icons/icon.svg'].map(path => new URL(path, self.registration.scope).href);
 const SHELL_URLS = new Set(SHELL);
