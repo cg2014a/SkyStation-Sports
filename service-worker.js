8ca4bc741a1eac90687f03c4274af18dfef4c27e
cg2014a <cgarrett4@hotmail.com>
2026-08-31 17:14:00 -0500

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'skystation-sports-v1.0.1';
+const VERSION = 'skystation-sports-v1.0.2';
 const SHELL = new Set(['/', '/index.html', '/style.css', '/app.js', '/manifest.webmanifest', '/icons/icon.svg']);
 self.addEventListener('install', event => event.waitUntil(caches.open(VERSION).then(cache => cache.addAll([...SHELL])).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('skystation-sports-') && key !== VERSION).map(key => caches.delete(key)))).then(() => self.clients.claim())));
