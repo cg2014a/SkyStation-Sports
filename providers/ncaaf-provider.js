2e2300e5ffbf764c3f3d35f2577f097d1b8140a4
cg2014a <cgarrett4@hotmail.com>
2026-09-03 19:10:24 -0500

    v34

--- providers/ncaaf-provider.js
+++ providers/ncaaf-provider.js
@@ -12,4 +12,6 @@ const normalize=event=>{
   return {...game,homeTeam:enrich(game.homeTeam),awayTeam:enrich(game.awayTeam)};
 };
 
-export default{league:'NCAAF',games:async date=>{const data=await endpoint(`${workerBase}/api/ncaaf/scores?date=${isoDate(date).replaceAll('-','')}`);return(data.events||[]).map(normalize)},standings:async()=>({}),teams:async()=>[]};
+const mapTeams=data=>(data.teams||[]).map(team=>({id:`NCAAF:${team.id}`,providerId:team.id,league:'NCAAF',name:team.displayName||team.name,abbreviation:team.abbreviation,logo:team.logo||null}));
+
+export default{league:'NCAAF',games:async date=>{const data=await endpoint(`${workerBase}/api/ncaaf/scores?date=${isoDate(date).replaceAll('-','')}`);return(data.events||[]).map(normalize)},standings:async()=>({}),teams:async()=>mapTeams(await endpoint(`${workerBase}/api/ncaaf/teams`))};
